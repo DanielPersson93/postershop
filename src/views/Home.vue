864e58fd93039fdc40fb4850e0e5b7693dbb45f0
Daniel Persson <daniel.persson93@hotmail.se>
2022-02-10 13:32:38 +0100

    added poster.vue code

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="home">
     <div class="grid">
-      <article class="card" v-for="poster in this.$store.state.posters" :key="poster.id">
-        <router-link :to="'/poster/' + poster.id">
-        <img :src="require(`../assets/${poster.picture}`)" alt="">
-        <h1>{{poster.title}}</h1>
-       </router-link>
-       <p>{{poster.description}}</p>
-       <button>Oh, take my money!</button>
+      <article
+        class="card"
+        v-for="poster in this.$store.state.posters"
+        :key="poster.id"
+      >
+        <router-link :to="{ name: 'Poster', params: { poster: poster } }">
+          <img :src="require(`../assets/${poster.picture}`)" alt="" />
+          <h1>{{ poster.title }}</h1>
+        </router-link>
+        <p>{{ poster.description }}</p>
+        <button>Oh, take my money!</button>
       </article>
-      
     </div>
   </div>
 </template>
@@ -19,41 +22,40 @@ export default {};
 </script>
 
 <style lang="scss" scoped>
-  .home{
-    background: black;
-    padding: 0 221px 126px
-  }
-  img{
-    width: 310px;
-  }
+.home {
+  background: black;
+  padding: 0 221px 126px;
+}
+img {
+  width: 310px;
+}
 
-  .grid{
-    display: grid;
-    grid-template-columns: repeat(3,1fr);
-  }
+.grid {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+}
 
-  article{
-    display: flex;
-    flex-direction: column;
-    width: 310px;
-    margin-top: 96px;
-  }
+article {
+  display: flex;
+  flex-direction: column;
+  width: 310px;
+  margin-top: 96px;
+}
 
-  a, p{
-    text-decoration: none;
-    color: white;
-  }
+a,
+p {
+  text-decoration: none;
+  color: white;
+}
 
-  button{
-    all: unset;
-    background: #E83F57;
-    padding: .8rem;
-    width: 50%;
-    color: white;
-    border-radius: 2px;
-    font-weight: bold;
-    cursor: pointer;
-  }
-
-
+button {
+  all: unset;
+  background: #e83f57;
+  padding: 0.8rem;
+  width: 50%;
+  color: white;
+  border-radius: 2px;
+  font-weight: bold;
+  cursor: pointer;
+}
 </style>
--- src/views/Poster.vue
+++ src/views/Poster.vue
@@ -1,5 +1,61 @@
 <template>
   <div class="about">
-    <h1>{{ poster.name }}</h1>
+    <img :src="require(`../assets/${poster.picture}`)" alt="" />
+    <div>
+      <h1>{{ poster.title }}</h1>
+      <p>{{ poster.description }}</p>
+      <p>Price: ${{ poster.price }}</p>
+      <p>Rating: {{ poster.rating }}/5</p>
+
+      <button>Oh, take my money!</button>
+    </div>
   </div>
 </template>
+<script>
+export default {
+  props: ["poster"],
+};
+</script>
+<style lang="scss" scoped>
+.about {
+  color: white;
+  display: flex;
+  img {
+    width: 500px;
+  }
+  div {
+    display: flex;
+    flex-direction: column;
+    width: 50%;
+    margin-left: 2rem;
+    h1 {
+      font-size: 3rem;
+      font-weight: bolder;
+    }
+
+    p {
+      margin-top: 0;
+
+      font-size: 1.5rem;
+    }
+
+    button {
+      all: unset;
+      margin-left: 0rem;
+      margin-top: 4rem;
+      padding: 1rem;
+      color: white;
+      background: #e83f57;
+      font-size: 1.5rem;
+      font-weight: bold;
+      align-self: flex-start;
+      border-radius: 0.5rem;
+      transition: ease-in 0.3s;
+    }
+    button:hover {
+      transition: ease-in 0.3s;
+      background: rgb(139, 0, 0);
+    }
+  }
+}
+</style>
