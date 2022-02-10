5ad929969cde5befb410af2e45ef090fd2bc419a
PeterKron <peter.kronqvist@hotmail.se>
2022-02-10 10:42:41 +0100

    footer done

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,24 @@
     <router-view/>
     <footer>
       <nav class="footersocialmedia">
-        <img src="./assets/logo-facebook.svg" alt="">
-        <img src="./assets/logo-instagram.svg" alt="">
-        <img src="./assets/logo-linkedin.svg" alt="">
-        <img src="./assets/logo-twitter.svg" alt="">
+        <div>
+          <h1>Nav</h1>
+          <a href="">Products</a>
+          <a href="">About</a>
+          <a href="">Contact</a>
+        </div>
+        <div>
+          <h1>Address</h1>
+          <p>Plinkeplonkvägen 32</p>
+          <p>888 81 Klonkberga</p>
+        </div>
+        <div>
+          <h1>Social media</h1>
+          <img src="./assets/logo-facebook.svg" alt="" width="32" height="32">
+          <img src="./assets/logo-instagram.svg" alt="" width="32" height="32">
+          <img src="./assets/logo-linkedin.svg" alt="" width="32" height="32">
+          <img src="./assets/logo-twitter.svg" alt="" width="32" height="32">
+        </div>
       </nav>
     </footer>
   </div>
@@ -18,11 +32,36 @@
 
 <style lang="scss">
 .footersocialmedia {
-  background-color: black
+  display: flex;
+  justify-content: space-around;
+  div {
+    margin-top: 50px;
+  }
+  div:first-of-type {
+    display: flex;
+    flex-direction: column;
+  }
+  img {
+    margin-right: 18px;
+  }
+  h1 {
+    line-height: 48px;
+    font-size: 32px;
+  }
+  a {
+    line-height: 150%;
+    color: white;
+    text-decoration: none;
+  }
+  color: white;
+  height: 320px;
+  background-color: rgba(255, 255, 255, 0.2);
 }
 
 @import url('https://fonts.googleapis.com/css2?family=Fira+Sans&display=swap');
 #app {
   font-family: 'Fira Sans', sans-serif;
+  background-color: black;
+  width: 1440px;
 }
 </style>
