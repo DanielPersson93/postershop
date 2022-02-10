618968360dacf464935c06973372292c626d8de7
Daniel Persson <daniel.persson93@hotmail.se>
2022-02-10 10:02:40 +0100

    fixed header in App

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,47 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <header>
+      <div class="header__block">
+        <h1 class="header__poster-shop">Poster Shop</h1>
+        <img class="header__ninja-img" src="./assets/ninjahead.svg" alt="" />
+      </div>
+    </header>
+    <router-view></router-view>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import url("https://fonts.googleapis.com/css2?family=PT+Mono&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Ropa+Sans:ital@0;1&display=swap");
+html,
+body {
+  margin: 0;
+  display: flex;
+  justify-content: center;
+
+  h1 {
+    font-family: "Roboto", sans-serif;
+  }
 }
+#app {
+  width: 1440px;
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+  header {
+    height: 608px;
+    background: #3de07e;
+    div {
+      height: 100%;
+      display: flex;
+      justify-content: space-between;
+      flex-direction: column;
+    }
+    h1 {
+      margin-top: 13rem;
+      font-size: 102px;
+      align-self: center;
+    }
+    img {
+      width: 20%;
+      align-self: center;
     }
   }
 }
