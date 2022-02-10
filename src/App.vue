3f7827aa7e73639e469ccf9dddba5cf626093d0a
PeterKron <peter.kronqvist@hotmail.se>
2022-02-10 11:17:35 +0100

    change scss in app

--- src/App.vue
+++ src/App.vue
@@ -31,10 +31,19 @@
 </template>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Fira+Sans&display=swap');
+  #app {
+    font-family: 'Fira Sans', sans-serif;
+    background-color: black;
+    width: 1440px;
+  }
+
 .footersocialmedia {
   display: flex;
   justify-content: space-around;
-  // align-items: center;
+    color: white;
+    height: 320px;
+    background-color: rgba(255, 255, 255, 0.2);
   div:first-of-type {
     display: flex;
     flex-direction: column;
@@ -51,15 +60,6 @@
     color: white;
     text-decoration: none;
   }
-  color: white;
-  height: 320px;
-  background-color: rgba(255, 255, 255, 0.2);
 }
 
-@import url('https://fonts.googleapis.com/css2?family=Fira+Sans&display=swap');
-#app {
-  font-family: 'Fira Sans', sans-serif;
-  background-color: black;
-  width: 1440px;
-}
 </style>
